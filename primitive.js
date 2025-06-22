ddd2afcef395876f544f43fa79da9c04f65d5908
Aman Bisht <aman.bisht1301@gmail.com>
2025-06-22 12:42:57 +0530

    primitive data type

---
+++ primitive.js
@@ -0,0 +1,12 @@
+// NN SS BB U
+// Null , Number , String , Symbol , Boolean , Big Int , Undefined
+
+let a = null; 
+let b  = 345;
+let c = "Aman";
+let d = Symbol (" I am a New Symbol");
+let e = true;
+let f = BigInt("42378");
+let g = undefined;
+
+console.log( a,b,c,d,e,f,g);
